d82af2ea11d58577ce74c203e315f806b8255bd7
CEM NAR <narcem67380@gmail.com>
2019-12-10 16:08:36 +0100

    quete 14

--- Readme.md
+++ Readme.md
@@ -1,6 +1,6 @@
-
-[Quete 09](https://www.loom.com/share/7d0777231519480fb0d05df8eb36dcc8)
-[Quete 10](https://www.loom.com/share/446ed6e8791b4e5c96dcfc017cca26b3)
-[Quete 11](https://www.loom.com/share/c6be977c6d134b11a084ee8af9aad18e)
-[Quete 12](https://www.loom.com/share/ca6d2947edc34c9f92f5856a156d6b3f)
-[Quete 13](https://www.loom.com/share/d219fd4baa9a4dbf9e09fd27720321a2)
+## **Lien des quetes**
+- [Quete 09](https://www.loom.com/share/7d0777231519480fb0d05df8eb36dcc8)
+- [Quete 10](https://www.loom.com/share/446ed6e8791b4e5c96dcfc017cca26b3)
+- [Quete 11](https://www.loom.com/share/c6be977c6d134b11a084ee8af9aad18e)
+- [Quete 12](https://www.loom.com/share/ca6d2947edc34c9f92f5856a156d6b3f)
+- [Quete 13](https://www.loom.com/share/d219fd4baa9a4dbf9e09fd27720321a2)
--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -3,9 +3,12 @@
 namespace App\Form;
 
 use App\Entity\Program;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use App\Entity\Actor;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class ProgramType extends AbstractType
 {
@@ -18,7 +21,13 @@ class ProgramType extends AbstractType
             ->add('country')
             ->add('year')
             ->add('category', null, ['choice_label' => 'name'])
-        ;
+            ->add('actors', EntityType::class, [
+                'class' => Actor::class,
+                'choice_label' => 'name',
+                'multiple'=> true,
+                'expanded'=> true,
+                'by_reference' => false,
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
